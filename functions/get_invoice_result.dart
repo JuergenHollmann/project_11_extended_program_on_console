ab8deae4300872a97f2f230fa2605fa83103cf2b
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-09-08 20:11:55 +0200

    made some notices

--- functions/get_invoice_result.dart
+++ functions/get_invoice_result.dart
@@ -1,3 +1,4 @@
+// get_invoice_result.dart
 double getInvoiceResult(double itemPrice, double quantity) {
   double taxPercent = 19;
   double taxSum;
@@ -20,32 +21,3 @@ double getInvoiceResult(double itemPrice, double quantity) {
   return totalSum; // auf 2 Stellen nach dem Komma gekürzt mit "toStringAsFixed(2)".
 }
 
-// Code für später:
-
-// class Invoice {
-//   double itemPrice;
-//   double taxSum;
-//   double totalSum;
-//   Invoice(this.itemPrice, this.taxSum, this.totalSum);
-//   double totalSumX() {
-//     // double totalsum = 0;
-//     // totalsum = totalsum * 3;
-//     // Customer gesamtUmsatz = customer[0];
-//     return totalSum * 0.19;
-//   }
-// }
-
-// Nur zum Testen:
-  // double itemPrice = 200;
-  // double quantity = 1;
-  // double taxSum = 0;
-  // double totalSum = 0;
-  // taxSum = itemPrice * quantity * 0.19;
-  // totalSum = itemPrice * quantity + taxSum;
-  // print("$totalSum €");
-  // print(
-  //     "---------------------------------------------------------------------");
-  // print("---------------------------------------");
-  // print("11. Multiplikation zweier Zahlen");
-  // print("Das Produkt ist ${productResult(2, 2)}");
-  // print("---------------------------------------");
