dbe2b76d4326f96f2045612b6e30a801430c38cd
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-09-08 20:12:30 +0200

    new solution to ask for a bool

--- functions/check_username.dart
+++ functions/check_username.dart
@@ -3,19 +3,29 @@
 Funktion: Überprüfen ob der Benutzername korrekt ist.
 -------------------------------------------------------------
 Eingabe:            Text: "Benutzername"
-Ausgabe:            Text: "Benutzername korrekt"
+Ausgabe:            wahr/falsch
 -------------------------------------------------------------
-Ausgabe_Datentyp:   String
-Funktions_Name:     checkUserName
+Ausgabe_Datentyp:   bool
+Funktions_Name:     functionCheckUserName
 Eingabe_Datentyp:   String
 Eingabe_Name:       inputUserName
 -----------------------------------------------------------*/
-String functionCheckUserName(String inputUserName){
+bool functionCheckUserName(String inputUserName){
   String checkUserName = "Jürgen";
+  bool result = false;
   if(checkUserName == inputUserName){
-    print("Der Benutzername \"$checkUserName\" wurde \u{1f600} korrekt \u{1f600} eingegeben!"); // \u{1f600} = 😀
-  }else{
-    print("Der Benutzername \"$inputUserName\" wurde NICHT korrekt eingegeben!");
+    result = true;
+    //print("Der Benutzername \"$checkUserName\" wurde \u{1f600} korrekt \u{1f600} eingegeben!"); // \u{1f600} = 😀
+  }else if (checkUserName != inputUserName){
+    result = false;
+    //print("Der Benutzername \"$inputUserName\" wurde NICHT korrekt eingegeben!");
   }
-  return checkUserName;
+  return result;
   }
+
+  bool oddEvenNumber(int num1) {
+  return (num1 % 2 == 0);
+}
+
+// String CheckUserNameInfoText =  ("${functionCheckUserName(inputUserName).contains("NICHT")}");
+// print(CheckUserNameInfoText);
