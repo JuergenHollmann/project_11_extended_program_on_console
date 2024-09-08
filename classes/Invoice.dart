11d4fddb3ea87c25d0184ffbcb79b36f6c47056a
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-09-08 12:48:35 +0200

    some changes in invoice.dart

---
+++ classes/Invoice.dart
@@ -0,0 +1,29 @@
+// Code für später:
+
+// class Invoice {
+//   double itemPrice;
+//   double taxSum;
+//   double totalSum;
+//   Invoice(this.itemPrice, this.taxSum, this.totalSum);
+//   double totalSumX() {
+//     // double totalsum = 0;
+//     // totalsum = totalsum * 3;
+//     // Customer gesamtUmsatz = customer[0];
+//     return totalSum * 0.19;
+//   }
+// }
+
+// Nur zum Testen:
+  // double itemPrice = 200;
+  // double quantity = 1;
+  // double taxSum = 0;
+  // double totalSum = 0;
+  // taxSum = itemPrice * quantity * 0.19;
+  // totalSum = itemPrice * quantity + taxSum;
+  // print("$totalSum €");
+  // print(
+  //     "---------------------------------------------------------------------");
+  // print("---------------------------------------");
+  // print("11. Multiplikation zweier Zahlen");
+  // print("Das Produkt ist ${productResult(2, 2)}");
+  // print("---------------------------------------");
